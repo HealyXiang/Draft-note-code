dec1b60199c3e70efa110b2a07b929608019d66f
li.xiang <li.xiang@shopee.com>
2023-01-29 17:48:16 +0800

    feat: add format

--- js/json-draft.js
+++ js/json-draft.js
@@ -1,42 +1,48 @@
 console.log(JSON.stringify({ x: 5, y: 6 }));
 // Expected output: "{"x":5,"y":6}"
 
-console.log(JSON.stringify([new Number(3), new String('false'), new Boolean(false)]));
+console.log(
+  JSON.stringify([new Number(3), new String("false"), new Boolean(false)])
+);
 // Expected output: "[3,"false",false]"
 
-console.log(JSON.stringify({ x: [10, undefined, function(){}, Symbol('')] }));
+console.log(JSON.stringify({ x: [10, undefined, function () {}, Symbol("")] }));
 // Expected output: "{"x":[10,null,null,null]}"
 
 console.log(JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)));
 // Expected output: ""2006-01-02T15:04:05.000Z""
 
-
 let obj = {
-    a: undefined,
-    b() {
-        return 'bbbb'
-    },
-    c: Symbol(),
-    e: 'normal'
-}
+  a: undefined,
+  b() {
+    return "bbbb";
+  },
+  c: Symbol(),
+  e: "normal",
+};
 
-console.log(JSON.stringify(obj))
+console.log(JSON.stringify(obj));
 // '{"e":"normal"}'
 
+let arr = [
+  undefined,
+  function aa() {
+    return 33333;
+  },
+  Symbol("dddd"),
+];
 
-let arr = [undefined, function aa() {return 33333}, Symbol('dddd')]
-
-console.log(JSON.stringify(arr))
+console.log(JSON.stringify(arr));
 // '[null,null,null]'
 
-JSON.stringify(function(){})
+JSON.stringify(function () {});
 // undefined
 
-JSON.stringify(undefined)
+JSON.stringify(undefined);
 // undefined
 
-JSON.stringify(Symbol())
+JSON.stringify(Symbol());
 // undefined
 
-JSON.stringify({[Symbol("foo")]: "foo"});
+JSON.stringify({ [Symbol("foo")]: "foo" });
 // '{}'
--- js/usual-js-code.js
+++ js/usual-js-code.js
@@ -4,30 +4,30 @@
 若在触发事件后的n秒内又触发了事件，则从新触发事件开始重新计时，n秒后执行。
 */
 
-function debounce(callback, duration = 5*1000, immediate = false) {
-    let timer = null
-    function resCb(params) {
-        if (!timer && immediate) {
-            // immediate为true，第一次触发事件，必须执行
-            callback(params)
-            timer = 1 // 标记timer，下次触发事件才会进入else逻辑
-        } else {
-            clearTimeout(timer)
-            timer = setTimeout(() => {
-                callback(params)
-                clearTimeout(timer)
-            }, duration)
-        }
+function debounce(callback, duration = 5 * 1000, immediate = false) {
+  let timer = null;
+  function resCb(params) {
+    if (!timer && immediate) {
+      // immediate为true，第一次触发事件，必须执行
+      callback(params);
+      timer = 1; // 标记timer，下次触发事件才会进入else逻辑
+    } else {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback(params);
+        clearTimeout(timer);
+      }, duration);
     }
+  }
 
-    return resCb
+  return resCb;
 }
 
 function onChange(arg1) {
-    console.log(arg1)
+  console.log(arg1);
 }
 
-let debouncedOnChange = debounce(onChange, 1000, true)
+let debouncedOnChange = debounce(onChange, 1000, true);
 
 // debouncedOnChange(1,99)
 // debouncedOnChange(2)
@@ -38,7 +38,6 @@ let debouncedOnChange = debounce(onChange, 1000, true)
 // debouncedOnChange(7)
 // debouncedOnChange(8,99)
 
-
 // 节流
 
 /* 
@@ -47,50 +46,49 @@ n秒内只执行一次，n秒内多次触发事件，只执行一次。会稀释
 
 // 定时器版本
 function throttle(event, duration = 5000) {
-    let gate = true
-    let timer = null
-    return function (params) {
-        gate && event(params)
-        gate = false;
-        setTimeout(() => {
-            gate = true
-            clearTimeout(timer)
-        }, duration)
-    }
+  let gate = true;
+  let timer = null;
+  return function (params) {
+    gate && event(params);
+    gate = false;
+    setTimeout(() => {
+      gate = true;
+      clearTimeout(timer);
+    }, duration);
+  };
 }
 
 // 时间戳版本
 function throttleWithTimer(event, duration = 5000) {
-    let lastCallTime = 0
-    return function (params) {
-        const nowTime = Date.now()
-        if (nowTime - lastCallTime >= duration) {
-            event(params)
-            lastCallTime = nowTime
-        }
+  let lastCallTime = 0;
+  return function (params) {
+    const nowTime = Date.now();
+    if (nowTime - lastCallTime >= duration) {
+      event(params);
+      lastCallTime = nowTime;
     }
+  };
 }
 
 // let throttleOnChange = throttle(onChange, 1000, true)
-let throttleOnChange = throttleWithTimer(onChange, 1000, true)
+let throttleOnChange = throttleWithTimer(onChange, 1000, true);
 
-
-throttleOnChange(1,99)
-throttleOnChange(2)
-throttleOnChange(3)
-throttleOnChange(4)
-throttleOnChange(5)
-throttleOnChange(6)
-throttleOnChange(7)
-throttleOnChange(8,99)
+throttleOnChange(1, 99);
+throttleOnChange(2);
+throttleOnChange(3);
+throttleOnChange(4);
+throttleOnChange(5);
+throttleOnChange(6);
+throttleOnChange(7);
+throttleOnChange(8, 99);
 
 setTimeout(() => {
-    throttleOnChange(1,99)
-    throttleOnChange(2)
-    throttleOnChange(3)
-    throttleOnChange(4)
-    throttleOnChange(5)
-    throttleOnChange(6)
-    throttleOnChange(7)
-    throttleOnChange(8,99)
-}, 1000)
+  throttleOnChange(1, 99);
+  throttleOnChange(2);
+  throttleOnChange(3);
+  throttleOnChange(4);
+  throttleOnChange(5);
+  throttleOnChange(6);
+  throttleOnChange(7);
+  throttleOnChange(8, 99);
+}, 1000);
