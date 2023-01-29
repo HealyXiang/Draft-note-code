6bd1c17f3b48a06aa6976644f21782a60bd67a51
li.xiang <li.xiang@shopee.com>
2023-01-29 17:50:46 +0800

    feat: add format config

--- js/json-draft.js
+++ js/json-draft.js
@@ -1,12 +1,10 @@
 console.log(JSON.stringify({ x: 5, y: 6 }));
 // Expected output: "{"x":5,"y":6}"
 
-console.log(
-  JSON.stringify([new Number(3), new String("false"), new Boolean(false)])
-);
+console.log(JSON.stringify([new Number(3), new String('false'), new Boolean(false)]));
 // Expected output: "[3,"false",false]"
 
-console.log(JSON.stringify({ x: [10, undefined, function () {}, Symbol("")] }));
+console.log(JSON.stringify({ x: [10, undefined, function () {}, Symbol('')] }));
 // Expected output: "{"x":[10,null,null,null]}"
 
 console.log(JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)));
@@ -15,10 +13,10 @@ console.log(JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)));
 let obj = {
   a: undefined,
   b() {
-    return "bbbb";
+    return 'bbbb';
   },
   c: Symbol(),
-  e: "normal",
+  e: 'normal',
 };
 
 console.log(JSON.stringify(obj));
@@ -29,7 +27,7 @@ let arr = [
   function aa() {
     return 33333;
   },
-  Symbol("dddd"),
+  Symbol('dddd'),
 ];
 
 console.log(JSON.stringify(arr));
@@ -44,5 +42,5 @@ JSON.stringify(undefined);
 JSON.stringify(Symbol());
 // undefined
 
-JSON.stringify({ [Symbol("foo")]: "foo" });
+JSON.stringify({ [Symbol('foo')]: 'foo' });
 // '{}'
